bff0472d5404d29ec86f2d143ddbae4df4bc40e7
Richard Li <richali@amazon.com>
2021-05-27 10:49:21 -0700

    Bump to plugin to 1.0

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -14,7 +14,6 @@ dependencyResolutionManagement {
                 }
             }
         }
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
         gradlePluginPortal()
         mavenCentral()
     }
